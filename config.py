a8a043d7f8ea44f9f4f4f84cbcbf19d0e65ef9ae
Philip MacMenamin <philip.macmenamin@nih.gov>
2021-11-22 23:48:28 +0000

    adding thumbnail gen / graphicmagick Dockerfile

--- config.py
+++ config.py
@@ -2,3 +2,5 @@ class Config:
     # location in container
     dm2mrc_loc = "/usr/local/IMOD/bin/dm2mrc"
     mrc2tif_loc = "/usr/local/IMOD/bin/mrc2tif"
+    size_lg = "1024x1024"
+    size_sm = "300x300"
--- dm_conversion/flow.py
+++ dm_conversion/flow.py
@@ -1,6 +1,6 @@
 from typing import Optional, List
 from pathlib import Path
-
+from prefect.engine import signals
 from prefect import Flow, task, Parameter, unmapped
 from prefect.triggers import always_run
 from prefect.tasks.docker.containers import (
@@ -34,6 +34,44 @@ class Container_Dm2mrc(CreateContainer):
         )
 
 
+class Container_gm_convert(CreateContainer):
+    gm = "/usr/bin/gm"
+    cmd = "gm convert"
+    sharpen = "2"
+    # docker run
+    # -v $(pwd)/test/input_files/:/io
+    # graphicsmagick gm convert
+    # -size 300x300 "/io/20210525_1416_A000_G000.jpeg"
+    # -resize 300x300 -sharpen 2 -quality 70 "/io/20210525_1416_A000_G000_sm.jpeg"
+
+    def run(self, input_dir: str, fp: Path, size: str):
+        if size == "sm":
+            scaler = Config.size_sm
+        elif size == "lg":
+            scaler = Config.size_lg
+        else:
+            raise signals.FAIL(f"Thumbnail must be either sm or lg, not {size}")
+        return super().run(
+            image_name="graphicsmagick",
+            volumes=[f"{input_dir}:/io"],
+            host_config={"binds": [input_dir + ":/io"]},
+            command=[
+                Container_gm_convert.gm,
+                "convert",
+                "-size",
+                scaler,
+                f"/io/{fp.stem}.jpeg",
+                "-resize",
+                scaler,
+                "-sharpen",
+                "2",
+                "-quality",
+                "70",
+                f"/io/{fp.stem}_{size}.jpeg",
+            ],
+        )
+
+
 class Container_Mrc2tif(CreateContainer):
     def run(self, input_dir: str, fp: Path):
         return super().run(
@@ -51,6 +89,8 @@ class Container_Mrc2tif(CreateContainer):
 
 create_mrc = Container_Dm2mrc()
 create_jpeg = Container_Mrc2tif()
+create_thumb = Container_gm_convert()
+
 start = StartContainer()
 wait = WaitOnContainer()
 logs = GetContainerLogs(trigger=always_run)
@@ -73,15 +113,26 @@ with Flow("dm_to_jpeg") as flow:
     input_dir = Parameter("input_dir")
     job = init_job(input_dir=input_dir)
     dm4_fps = get_files(job, "dm4")
+
     dm_container_ids = create_mrc.map(input_dir=unmapped(input_dir), fp=dm4_fps)
     dm_container_starts = start.map(dm_container_ids)
     status_code = wait.map(dm_container_ids)
+
     jpeg_container_ids = create_jpeg.map(
         input_dir=unmapped(input_dir), fp=dm4_fps, upstream_tasks=[status_code]
     )
     jpeg_container_starts = start.map(jpeg_container_ids)
     jpeg_status_codes = wait.map(jpeg_container_ids)
-#    logs = logs(_id, upstream_tasks=[status_code])
 
-# input_dir = "/home/macmenaminpe/code/image_portal_workflows/test/input_files/"
-# flow.run(input_dir=input_dir)
+    thumb_container_ids = create_thumb.map(
+        input_dir=unmapped(input_dir), fp=dm4_fps, size=unmapped("sm")
+    )
+    thumb_container_starts = start.map(thumb_container_ids)
+    thumb_status_codes = wait.map(thumb_container_ids)
+
+    thumb_container_ids_lg = create_thumb.map(
+        input_dir=unmapped(input_dir), fp=dm4_fps, size=unmapped("lg")
+    )
+    thumb_container_starts = start.map(thumb_container_ids_lg)
+    thumb_status_codes = wait.map(thumb_container_ids_lg)
+#    logs = logs(_id, upstream_tasks=[status_code])
