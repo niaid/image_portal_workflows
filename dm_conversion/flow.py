89d209a42488cd8f61e0692a256b8826689ae535
Philip MacMenamin <philip.macmenamin@nih.gov>
2021-11-24 20:09:05 +0000

    test no gm

--- dm_conversion/flow.py
+++ dm_conversion/flow.py
@@ -140,29 +140,29 @@ with Flow("dm_to_jpeg") as flow:
     jpeg_container_starts = start.map(jpeg_container_ids)
     jpeg_status_codes = wait.map(jpeg_container_ids)
 
-    small_thumb_locs = gen_output_fp.map(
-        input_fp=jpeg_locs, output_ext=unmapped("_SM.jpeg")
-    )
-    thumb_container_ids_sm = create_thumb.map(
-        input_dir=unmapped(input_dir),
-        fp=jpeg_locs,
-        output_fp=small_thumb_locs,
-        size=unmapped("sm"),
-        upstream_tasks=[jpeg_status_codes],
-    )
-    thumb_container_starts_sm = start.map(thumb_container_ids_sm)
-    thumb_status_codes_sm = wait.map(thumb_container_ids_sm)
-
-    large_thumb_locs = gen_output_fp.map(
-        input_fp=jpeg_locs, output_ext=unmapped("_LG.jpeg")
-    )
-    thumb_container_ids_lg = create_thumb.map(
-        input_dir=unmapped(input_dir),
-        fp=jpeg_locs,
-        output_fp=large_thumb_locs,
-        size=unmapped("lg"),
-        upstream_tasks=[jpeg_status_codes],
-    )
-    thumb_container_starts_lg = start.map(thumb_container_ids_lg)
-    thumb_status_codes_lg = wait.map(thumb_container_ids_lg)
+#    small_thumb_locs = gen_output_fp.map(
+#        input_fp=jpeg_locs, output_ext=unmapped("_SM.jpeg")
+#    )
+#    thumb_container_ids_sm = create_thumb.map(
+#        input_dir=unmapped(input_dir),
+#        fp=jpeg_locs,
+#        output_fp=small_thumb_locs,
+#        size=unmapped("sm"),
+#        upstream_tasks=[jpeg_status_codes],
+#    )
+#    thumb_container_starts_sm = start.map(thumb_container_ids_sm)
+#    thumb_status_codes_sm = wait.map(thumb_container_ids_sm)
+#
+#    large_thumb_locs = gen_output_fp.map(
+#        input_fp=jpeg_locs, output_ext=unmapped("_LG.jpeg")
+#    )
+#    thumb_container_ids_lg = create_thumb.map(
+#        input_dir=unmapped(input_dir),
+#        fp=jpeg_locs,
+#        output_fp=large_thumb_locs,
+#        size=unmapped("lg"),
+#        upstream_tasks=[jpeg_status_codes],
+#    )
+#    thumb_container_starts_lg = start.map(thumb_container_ids_lg)
+#    thumb_status_codes_lg = wait.map(thumb_container_ids_lg)
 #    logs = logs(_id, upstream_tasks=[status_code])
