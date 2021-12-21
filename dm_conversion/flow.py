c65618eaaf91e3c26df61378acff7118dc885218
Philip MacMenamin <philip.macmenamin@nih.gov>
2021-12-21 23:40:07 +0000

    leave out lg conv

--- dm_conversion/flow.py
+++ dm_conversion/flow.py
@@ -177,19 +177,19 @@ with Flow("dm_to_jpeg") as flow:
     thumb_container_starts_sm = startGM.map(thumb_container_ids_sm)
     thumb_status_codes_sm = waitGM.map(thumb_container_ids_sm)
 
-    # size dow jpegs for large thumbs
-    large_thumb_locs = gen_output_fname.map(
-        input_fp=jpeg_locs, output_ext=unmapped("_LG.jpeg")
-    )
-    thumb_container_ids_lg = create_thumb.map(
-        input_dir=unmapped(input_dir),
-        fp=jpeg_locs,
-        output_fp=large_thumb_locs,
-        size=unmapped("lg"),
-        upstream_tasks=[jpeg_status_codes],
-    )
-    thumb_container_starts_lg = startGMlg.map(thumb_container_ids_lg)
-    thumb_status_codes_lg = waitGMlg.map(
-        thumb_container_ids_lg, upstream_tasks=[thumb_container_starts_lg]
-    )
+#    # size dow jpegs for large thumbs
+#    large_thumb_locs = gen_output_fname.map(
+#        input_fp=jpeg_locs, output_ext=unmapped("_LG.jpeg")
+#    )
+#    thumb_container_ids_lg = create_thumb.map(
+#        input_dir=unmapped(input_dir),
+#        fp=jpeg_locs,
+#        output_fp=large_thumb_locs,
+#        size=unmapped("lg"),
+#        upstream_tasks=[jpeg_status_codes],
+#    )
+#    thumb_container_starts_lg = startGMlg.map(thumb_container_ids_lg)
+#    thumb_status_codes_lg = waitGMlg.map(
+#        thumb_container_ids_lg, upstream_tasks=[thumb_container_starts_lg]
+#    )
     # logs = logs(_id, upstream_tasks=[status_code])
