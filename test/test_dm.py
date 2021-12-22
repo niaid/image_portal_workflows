35e0dc86c0dbf01a0f5c82f8c5be8e339ab6be7f
Philip MacMenamin <philip.macmenamin@nih.gov>
2021-12-22 17:58:19 +0000

    allow single files to be run within Sample

--- test/test_dm.py
+++ test/test_dm.py
@@ -1,6 +1,9 @@
 import pytest
 import sys
 
+from prefect.engine import signals
+from prefect.tasks.prefect import create_flow_run
+
 sys.path.append("..")
 from image_portal_workflows.config import Config
 from image_portal_workflows.dm_conversion.flow import flow
@@ -17,3 +20,17 @@ def test_dm4_conv(mock_nfs_mount):
         input_dir=os.getcwd() + "/test/input_files/",
     )
     assert result.is_successful()
+
+
+def test_input_fname(mock_nfs_mount):
+    flow.run(
+        input_dir=os.getcwd() + "/test/input_files/",
+        file_name="20210525_1416_A000_G000.dm4",
+    )
+
+
+def test_single_file_not_found_gens_exception(mock_nfs_mount):
+    state = flow.run(
+        input_dir=os.getcwd() + "/test/input_files/", file_name="does_not_exist"
+    )
+    assert state.is_failed()
