6d16739d8b58ec6b970434da400a983e599c876b
Philip MacMenamin <philip.macmenamin@nih.gov>
2022-03-18 19:59:38 +0000

    Adds first attempt at dimension command

--- brt/flow.py
+++ brt/flow.py
@@ -7,6 +7,7 @@ import tempfile
 import prefect
 from jinja2 import Environment, FileSystemLoader
 from prefect import task, Flow, Parameter
+from prefect.engine import signals
 from prefect.tasks.shell import ShellTask
 
 
@@ -28,9 +29,9 @@ def make_work_dir() -> Path:
 @task
 def prep_adoc(working_dir: Path, fname: Path) -> Path:
     """copy the template adoc file to the working_dir, and add vars"""
-    cwd = os.path.join(os.path.dirname(__file__), '..')
+    repo_dir = os.path.join(os.path.dirname(__file__), '..')
     adoc_fp = f"{working_dir}/{fname.stem}.adoc_template"
-    shutil.copyfile(f"{cwd}/templates/dirTemplate.adoc", adoc_fp)
+    shutil.copyfile(f"{repo_dir}/templates/dirTemplate.adoc", adoc_fp)
     return Path(adoc_fp)
 
 
@@ -51,19 +52,21 @@ def prep_input_fp(fname: Path, working_dir: Path) -> Path:
 
 
 @task
-def update_adoc(adoc_fp: Path) -> str:
+def update_adoc(adoc_fp: Path) -> Path:
+    """updates the adoc file with input params, TODO"""
     file_loader = FileSystemLoader(str(adoc_fp.parent))
     env = Environment(loader=file_loader)
     template = env.get_template(adoc_fp.name)
     vals = {
         "basename": adoc_fp.stem,
-        "bead_size": 4,
-        "montage": 0,
-        "input_dir": str(adoc_fp.parent),
+        "bead_size": 10,
         "pixel_size": 4,
         "newstack_bin_by_fact": 4,
-        "light_beads": 4,
-        "tilt_thickness": 2,
+        "light_beads": 0,
+        "tilt_thickness": 256,
+        "runtime_bin_by_fact": 4,
+        "montage": 0,
+        "dataset_dir": str(adoc_fp.parent),
     }
 
     output = template.render(vals)
@@ -72,10 +75,39 @@ def update_adoc(adoc_fp: Path) -> str:
          print(output, file=_file)
     logger = prefect.context.get("logger")
     logger.info(f"created {adoc_loc}")
-    cmd = f"{Config.brt_binary} -di {adoc_loc.as_posix()} -cp 8 -gpu 1"
+    return adoc_loc
+
+@task
+def create_brt_command(adoc_fp: Path) -> str:
+    logger = prefect.context.get("logger")
+    cmd = f"{Config.brt_binary} -di {adoc_fp.as_posix()} -cp 8 -gpu 1"
     logger.info(cmd)
-    # cmd = f"ls {adoc_loc.as_posix()}"
     return cmd
+
+@task
+def gen_dimension_command(adoc_fp: Path) -> str:
+    logger = prefect.context.get("logger")
+    ali_file = Path(f"{adoc_fp.parent}/{adoc_fp.stem}_ali.mrv")
+    # formatted=($(header -s EBOV_VSV_1_preali.mrc))
+    # dim=($(header -s EBOV_VSV_1_preali.mrc))
+    cmd = f"header -s {ali_file}"
+    logger.info(cmd)
+    return cmd
+
+@task
+def check_brt_run_ok(adoc_fp: Path):
+    """
+    ensures the following files exist:
+    BASENAME_rec.mrc - the source for the reconstruction movie
+    BASENAME_full_rec.mrc - the source for the Neuroglancer pyramid
+    BASENAME_ali.mrc 
+    """
+    rec_file = Path(f"{adoc_fp.parent}/{adoc_fp.stem}_rec.mrc")
+    full_rec_file = Path(f"{adoc_fp.parent}/{adoc_fp.stem}_full_rec.mrc")
+    ali_file = Path(f"{adoc_fp.parent}/{adoc_fp.stem}_ali.mrc")
+    for _file in [rec_file, full_rec_file, ali_file]:
+        if not _file.exists():
+            raise signals.FAIL(f"File {_file} does not exist. BRT run failure.")
 
 
 with Flow("brt_flow", executor=Config.SLURM_EXECUTOR) as flow:
@@ -88,7 +120,11 @@ with Flow("brt_flow", executor=Config.SLURM_EXECUTOR) as flow:
     working_dir = make_work_dir()
     adoc_fp = prep_adoc(working_dir, fname)
     updated_adoc = update_adoc(adoc_fp)
+    brt_command = create_brt_command(adoc_fp=updated_adoc)
     input_fp = prep_input_fp(fname=fname, working_dir=working_dir)
-    c = shell_task(command=updated_adoc, upstream_tasks=[input_fp])
-# # contents = shell_task(command=f"{Config.brt_binary} -di {adoc_fp} -cp 8 -gpu 1")
-# print_l(str(adoc_fp))
+    brt = shell_task(command=brt_command, upstream_tasks=[input_fp])
+    check_brt_run_ok(adoc_fp=updated_adoc)
+    dimensions_cmd = gen_dimension_command(adoc_fp=updated_adoc, upstream_tasks=[brt])
+    dimensions = shell_task(command=dimensions_cmd)
+
+
