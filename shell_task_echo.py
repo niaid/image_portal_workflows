809240166068b1e1a7e11ccd0aa4bf922ec97f3c
Philip MacMenamin <philip.macmenamin@nih.gov>
2022-04-22 19:48:29 +0100

    testing local log func

--- shell_task_echo.py
+++ shell_task_echo.py
@@ -2,6 +2,7 @@ from prefect.tasks.shell import ShellTask
 import prefect
 from typing import List, Union, Optional
 from prefect.utilities.tasks import defaults_from_attrs
+from pathlib import Path
 
 
 def echo(command):
@@ -16,10 +17,14 @@ class ShellTaskEcho(ShellTask):
         env: dict = None,
         helper_script: str = None,
         to_echo: str = None,
+        to_log_local: Path = None,
     ) -> Optional[Union[str, List[str]]]:
 
         if to_echo:
             logger = prefect.context.get("logger")
             logger.info(to_echo)
+        if to_log_local and command:
+            with open("{to_log_local.parent}/local_log.txt", "w") as _file:
+                _file.write(command)
         super().run(command=command, env=env, helper_script=helper_script)
 
