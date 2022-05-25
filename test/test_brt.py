8e607aaa8d4e601e98ef13c49bfb4d6cf24e35a7
Philip MacMenamin <philip.macmenamin@nih.gov>
2022-05-25 15:37:24 +0100

    adding complete BRT wf test, with mocked out BRT steps

--- test/test_brt.py
+++ test/test_brt.py
@@ -1,36 +1,54 @@
+from typing import List
 import pytest
 import sys
 import tempfile
 import os
+import prefect
+from prefect import task
 from pathlib import Path
 from prefect.executors import LocalExecutor
 
 sys.path.append("..")
 
 from image_portal_workflows.config import Config
+from image_portal_workflows.utils import utils
+
+# from image_portal_workflows.brt import flow
 
 
 @pytest.fixture
 def hpc_env(monkeypatch):
     monkeypatch.setattr(Config, "SLURM_EXECUTOR", LocalExecutor())
-    monkeypatch.setattr(Config, "proj_dir", os.getcwd())
-    # monkeypatch.setattr(Config, "assets_dir", tempfile.mkdtemp())
+    monkeypatch.setattr(Config, "proj_dir", "/home/macmenaminpe/data/")
+    monkeypatch.setattr(Config, "mount_point", "/home/macmenaminpe/")
     monkeypatch.setattr(Config, "tmp_dir", "/tmp/")
-    monkeypatch.setattr(Config, "brt_binary", "echo ")
 
+    @task
+    def _create_brt_command(adoc_fp: Path) -> str:
+        s = f"cp {Config.proj_dir}/brt_run_keep/2013-1220-dA30_5-BSC-1_10*.mrc {adoc_fp.parent}"
+        prefect.context.get("logger").info(f"MOCKED {s}")
+        return s
 
-# def test_brt(hpc_env, monkeypatch):
-#    def mockreturn(self):
-#        return Path(os.getcwd() + "/test/input_files/PubChem-522220-bas-color.wrl")
-#
-# monkeypatch.setattr(Job, "get_file", mockreturn)
+    monkeypatch.setattr(utils, "create_brt_command", _create_brt_command)
 
 
 def test_dm4_conv(hpc_env):
-    from image_portal_workflows.brt.flow import flow
+    from image_portal_workflows.brt.flow import f
 
-    result = flow.run(
-        input_dir="/test/input_files/",
+    result = f.run(
+        adoc_template="dirTemplate",
+        dual="0",
+        montage="0",
+        gold="15",
+        focus="0",
+        bfocus="0",
+        fiducialless="1",
+        trackingMethod=None,
+        TwoSurfaces="0",
+        TargetNumberOfBeads="20",
+        LocalAlignments="0",
+        THICKNESS="30",
+        input_dir="/brt_run/Projects/single_mrc/",
         token="the_token",
         sample_id="the_sample_id",
         callback_url="https://ptsv2.com/t/",
