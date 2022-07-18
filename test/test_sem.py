5c276f0877e576546c50fb7ca39f0eab36d8ba7e
Philip MacMenamin <philip.macmenamin@nih.gov>
2022-07-18 22:56:56 +0100

    Adding test to run SEM workflow

---
+++ test/test_sem.py
@@ -0,0 +1,38 @@
+import pytest
+import sys
+from prefect.executors import LocalExecutor
+
+sys.path.append("..")
+import os
+
+
+@pytest.fixture
+def mock_nfs_mount(monkeypatch):
+    from image_portal_workflows.config import Config
+
+    monkeypatch.setattr(Config, "mount_point", os.getcwd() + "/test/input_files")
+    monkeypatch.setattr(Config, "proj_dir", os.getcwd())
+    monkeypatch.setattr(Config, "assets_dir", os.getcwd())
+    monkeypatch.setattr(Config, "tmp_dir", os.getcwd() + "/tmp")
+    monkeypatch.setattr(Config, "SLURM_EXECUTOR", LocalExecutor())
+    monkeypatch.setattr(Config, "xfalign_loc", "/usr/local/IMOD/bin/xfalign")
+    monkeypatch.setattr(Config, "tif2mrc_loc", "/usr/local/IMOD/bin/tif2mrc")
+    monkeypatch.setattr(Config, "mrc2tif_loc", "/usr/local/IMOD/bin/mrc2tif")
+    monkeypatch.setattr(Config, "xftoxg_loc", "/usr/local/IMOD/bin/xftoxg")
+    monkeypatch.setattr(Config, "newstack_loc", "/usr/local/IMOD/bin/newstack")
+    monkeypatch.setattr(Config, "convert_loc", "/usr/bin/convert")
+
+    # from image_portal_workflows.utils.utils import _add_outputs, _gen_callback_file_list
+
+
+def test_sem(mock_nfs_mount):
+    from image_portal_workflows.sem_tomo.flow import flow
+
+    result = flow.run(
+        input_dir="/test/input_files/sem_inputs/Projects/",
+        tilt_parameter="30.2",
+        token="the_token",
+        sample_id="the_sample_id",
+        callback_url="https://ptsv2.com/t/",
+    )
+    assert result.is_successful()
