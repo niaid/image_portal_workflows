97e098cb2dd979a1baab79f854dae9cc11ab98bc
Mike Bopf <mbopf@mail.nih.gov>
2023-03-13 15:31:25 -0400

    Renamed some stuff.

--- test/test_brt.py
+++ test/test_brt.py
@@ -20,10 +20,10 @@ def hpc_env(monkeypatch):
     def _mock_assets_dir(env: str) -> str:
         return os.getcwd()
 
-    def _get_cache_dir() -> str:
+    def _mock_test_output_dir() -> str:
         """
-        Get cached partial results based on machine type. Alternately could use a HOME env. variable.
-        :return: The full path where cached files are stored
+        Get partial results based on machine type. Alternately could use a HOME env. variable.
+        :return: The full path where the previously-created output files are stored
         """
         if platform.system() == 'Linux':
             return "/home/macmenaminpe/code/image_portal_workflows/test/input_files/brt_outputs/"
@@ -37,7 +37,7 @@ def hpc_env(monkeypatch):
     monkeypatch.setattr(Config, "tmp_dir", "/tmp/")
 
     @task
-    def _run_brt(
+    def _prep_mock_brt_run(
             file_path: FilePath,
             adoc_template: str,
             montage: int,
@@ -51,16 +51,16 @@ def hpc_env(monkeypatch):
             THICKNESS: int,
             ) -> None:
         prefect.context.get("logger").info(f"MOCKED {file_path}")
-        shutil.copy(os.path.join(_get_cache_dir(), "2013-1220-dA30_5-BSC-1_10_ali.mrc"), file_path.working_dir)
-        shutil.copy(os.path.join(_get_cache_dir(), "2013-1220-dA30_5-BSC-1_10_rec.mrc"), file_path.working_dir)
+        shutil.copy(os.path.join(_mock_test_output_dir(), "2013-1220-dA30_5-BSC-1_10_ali.mrc"), file_path.working_dir)
+        shutil.copy(os.path.join(_mock_test_output_dir(), "2013-1220-dA30_5-BSC-1_10_rec.mrc"), file_path.working_dir)
 
     @task
     def _create_brt_command(adoc_fp: Path) -> str:
-        s = f"cp {_get_cache_dir()}*.mrc {adoc_fp.parent}"
+        s = f"cp {_mock_test_output_dir()}*.mrc {adoc_fp.parent}"
         prefect.context.get("logger").info(f"MOCKED {s}")
         return s
 
-    monkeypatch.setattr(utils,'run_brt', _run_brt)
+    monkeypatch.setattr(utils,'run_brt', _prep_mock_brt_run)
 
 
 def test_brt(hpc_env):
