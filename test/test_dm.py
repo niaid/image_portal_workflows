46bb8e83d2756afe9e100b1b50f7fe2bbab525ea
Philip MacMenamin <philip.macmenamin@nih.gov>
2023-03-20 15:04:57 +0000

    hardcode paths for now

--- test/test_dm.py
+++ test/test_dm.py
@@ -21,10 +21,10 @@ def mock_nfs_mount(monkeypatch):
     monkeypatch.setattr(Config, "mount_point", os.getcwd() + "/test/input_files")
     monkeypatch.setattr(Config, "tmp_dir", "/tmp")
     monkeypatch.setattr(Config, "SLURM_EXECUTOR", LocalExecutor())
-    monkeypatch.setattr(Config, "header_loc", _cmd_loc("header"))
-    monkeypatch.setattr(Config, "dm2mrc_loc", _cmd_loc("dm2mrc"))
-    monkeypatch.setattr(Config, "mrc2tif_loc", _cmd_loc("mrc2tif"))
-    monkeypatch.setattr(Config, "newstack_loc", _cmd_loc("newstack"))
+    monkeypatch.setattr(Config, "header_loc", "/usr/local/IMOD/header")
+    monkeypatch.setattr(Config, "dm2mrc_loc", "/usr/local/IMOD/dm2mrc")
+    monkeypatch.setattr(Config, "mrc2tif_loc", "/usr/local/IMOD/mrc2tif")
+    monkeypatch.setattr(Config, "newstack_loc", "/usr/local/IMOD/newstack")
 
 
 def test_dm4_conv(mock_nfs_mount):
