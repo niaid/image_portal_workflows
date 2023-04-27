294395925d7e3da6630aa581d60dcad7eeb50f22
Philip MacMenamin <philip.macmenamin@nih.gov>
2023-04-27 19:48:31 +0100

    adding logging support for pytools

--- em_workflows/utils/neuroglancer.py
+++ em_workflows/utils/neuroglancer.py
@@ -10,7 +10,9 @@ from pathlib import Path
 import prefect
 from prefect import task
 from pytools.workflow_functions import visual_min_max
-
+import pytools
+import logging
+import sys
 
 @task
 def gen_zarr(fp_in: FilePath, width: int, height: int, depth: int=None) -> Dict:
@@ -52,8 +54,6 @@ def gen_zarr(fp_in: FilePath, width: int, height: int, depth: int=None) -> Dict:
         "1",
         "--tile_width",
         str(width),
-	"--max_workers", 
-	"50",
         "--tile_height",
         str(height)
     ]
@@ -73,64 +73,18 @@ def gen_zarr(fp_in: FilePath, width: int, height: int, depth: int=None) -> Dict:
     FilePath.run(cmd=cmd_ms, log_file=log_file)
     asset_fp = fp_in.copy_to_assets_dir(fp_to_cp=zarr)
     first_zarr_arr = Path(zarr.as_posix() + "/0")
+    pytools.logger.level = logging.DEBUG
+    BASIC_FORMAT = "%(levelname)s:%(name)s:%(message)s"
+    formatter = logging.Formatter(BASIC_FORMAT)
+    handler = logging.StreamHandler(stream=sys.stdout)
+    handler.setFormatter(formatter)
+    pytools.logger.addHandler(handler)
+
     metadata = visual_min_max(mad_scale=5, input_image=first_zarr_arr)
     ng_asset = fp_in.gen_asset(asset_type="neuroglancerPrecomputed", asset_fp=asset_fp)
 
     ng_asset["metadata"] = metadata
     return ng_asset
-
-#  @task
-#  def gen_pyramids(fp_in: FilePath) -> Dict:
-#      """
-#      converts nifti files into pyramid files.
-#      volume-to-precomputed-pyramid --downscaling-method=average --flat path/basename.nii path/neuro-basename
-#      """
-#      # generate pyramid dir
-#      outdir = fp_in.gen_output_fp(output_ext=f"/neuro-{fp_in.fp_in.stem}")
-#      utils.log(f"Created pyramid outdir {outdir}")
-#      nifti = fp_in.gen_output_fp(output_ext=".nii")
-#      if outdir.exists():
-#          shutil.rmtree(outdir)
-#          prefect.context.get("logger").info(
-#              f"Pyramid dir {outdir} already exists, overwriting."
-#          )
-#      log_file = f"{nifti.parent}/volume_to_precomputed_pyramid.log"
-#      cmd = [
-#          "volume-to-precomputed-pyramid",
-#          "--downscaling-method=average",
-#          "--flat",
-#          "--no-gzip",
-#          nifti.as_posix(),
-#          outdir.as_posix(),
-#      ]
-#      utils.log(f"Created {cmd}")
-#      FilePath.run(cmd=cmd, log_file=log_file)
-#      asset_fp = fp_in.copy_to_assets_dir(fp_to_cp=outdir)
-#      metadata = gen_metadata(fp_in=fp_in)
-#      ng_asset = fp_in.gen_asset(asset_type="neuroglancerPrecomputed", asset_fp=asset_fp)
-#      ng_asset["metadata"] = metadata
-#      return ng_asset
-
-
-#  def gen_metadata(fp_in: FilePath) -> Dict:
-#      min_max = fp_in.gen_output_fp(output_ext=f"_min_max.json")
-#      # first (/0) elt of image zarr array is used to gen stats
-#      zarr = fp_in.gen_output_fp(output_ext=".zarr/0")
-#      log_file = f"{zarr.parent}/mrc_visual_min_max.log"
-#      cmd = [
-#          "mrc_visual_min_max",
-#          zarr.as_posix(),
-#          "--mad",
-#          "5",
-#          "--output-json",
-#          min_max.as_posix(),
-#      ]
-#      utils.log(f"Created {cmd}")
-#      FilePath.run(cmd=cmd, log_file=log_file)
-#      with open(min_max, "r") as _file:
-#          metadata = json.load(_file)
-#      return metadata
-
 
 
 #  @task
@@ -144,14 +98,6 @@ def gen_zarr(fp_in: FilePath, width: int, height: int, depth: int=None) -> Dict:
 #      return outdir
 
 
-#  @task
-#  def gen_pyramid_cmd(fp: Path, outdir: Path) -> str:
-#      """
-#      volume-to-precomputed-pyramid --downscaling-method=average --flat path/basename.nii path/neuro-basename
-#      """
-#      cmd = f"volume-to-precomputed-pyramid --downscaling-method=average --flat {fp.parent}/{fp.stem}.nii {outdir} &> {fp.parent}/volume_to_precomputed_pyramid.log"
-#      prefect.context.get("logger").info(f"pyramid command: {cmd}")
-#      return cmd
 
 
 #  @task
@@ -167,47 +113,3 @@ def gen_zarr(fp_in: FilePath, width: int, height: int, depth: int=None) -> Dict:
 #      logger.info(f"compressing : {cmd}")
 #      subprocess.run(cmd, cwd=file_path.working_dir)
 
-
-@task
-def gen_min_max_cmd(fp: Path, out_fp: Path) -> str:
-    """
-    mrc_visual_min_max {basename}.nii --mad 5 --output-json mrc2ngpc-output.json
-    mad == Median absolute deviation - determined empiricaly 5
-    """
-    cmd = f"mrc_visual_min_max {fp.parent}/{fp.stem}.nii --mad 5 --output-json {out_fp} &> {fp.parent}/mrc_visual_min_max.log"
-    logger = prefect.context.get("logger")
-    logger.info(f"gen_min_max_cmd command: {cmd}")
-    return cmd
-
-
-@task
-def parse_min_max_file(fp: Path) -> Dict[str, str]:
-    """
-    min max command is run as a subprocess and dumped to a file.
-    This file needs to be parsed.
-    Should be four keys:
-    - neuroglancerPrecomputedMin,
-    - neuroglancerPrecomputedMax,
-    - neuroglancerPrecomputedFloor,
-    - neuroglancerPrecomputedLimit
-    Values should be ints.
-    Round min and floor: down, round max and limit: up.
-    """
-    with open(fp, "r") as _file:
-        metadata = json.load(_file)
-        # kv = json.load(_file)
-    #    _floor = kv["neuroglancerPrecomputedFloor"]
-    #    _limit = kv["neuroglancerPrecomputedLimit"]
-    #    _min = kv["neuroglancerPrecomputedMin"]
-    #    _max = kv["neuroglancerPrecomputedMax"]
-    #    _floor = kv.find("neuroglancerPrecomputedFloor")
-    #    _limit = kv.find("neuroglancerPrecomputedLimit")
-    #    _min = kv.find("neuroglancerPrecomputedMin")
-    #    _max = kv.find("neuroglancerPrecomputedMax")
-    #    metadata = {
-    #            "neuroglancerPrecomputedFloor": str(math.floor(_floor)),
-    #            "neuroglancerPrecomputedMin": str(math.floor(_min)),
-    #            "neuroglancerPrecomputedLimit": str(math.ceil(_limit)),
-    #            "neuroglancerPrecomputedMax": str(math.ceil(_max)),
-    #        }
-    return metadata
