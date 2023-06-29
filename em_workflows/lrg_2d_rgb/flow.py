070528bb314d93a50f7f246c74f0a7790a541593
Mike Bopf <mbopf@mail.nih.gov>
2023-06-29 16:07:36 -0400

    Made Prefect 2 changes for Lrg_2D pipeline

--- em_workflows/lrg_2d_rgb/flow.py
+++ em_workflows/lrg_2d_rgb/flow.py
@@ -2,11 +2,10 @@ from pathlib import Path
 from em_workflows.file_path import FilePath
 import pytools
 import SimpleITK
-from prefect import Flow, task, Parameter
+from prefect import flow, task
 
 from em_workflows.utils import utils
 from em_workflows.config import Config
-from prefect.run_configs import LocalRun
 
 
 @task
@@ -108,23 +107,21 @@ def gen_thumb(file_path: FilePath):
     return [thumb_asset, keyImage_asset]
 
 
-with Flow(
-    "lrg_2d_color",
-    state_handlers=[utils.notify_api_completion, utils.notify_api_running],
-    executor=Config.SLURM_EXECUTOR,
-    run_config=LocalRun(labels=[utils.get_environment()]),
-) as flow:
+@flow
+def lrg_2d_flow(
+    input_dir: str,
+    file_name: str = None,
+    callback_url: str = None,
+    token: str = None,
+    no_api: bool = None,
+    # keep workdir if set true, useful to look at outputs
+    keep_workdir: bool = False,
+):
     """
     -list all png inputs (assumes all are "large")
     -create tmp dir for each.
     -convert to tiff -> zarr -> jpegs (thumb)
     """
-    input_dir = Parameter("input_dir")
-    file_name = Parameter("file_name", default=None)
-    callback_url = Parameter("callback_url", default=None)()
-    token = Parameter("token", default=None)()
-    no_api = Parameter("no_api", default=None)()
-    keep_workdir = Parameter("keep_workdir", default=False)()
     input_dir_fp = utils.get_input_dir(input_dir=input_dir)
 
     input_fps = utils.list_files(
@@ -134,8 +131,8 @@ with Flow(
     )
     fps = utils.gen_fps(input_dir=input_dir_fp, fps_in=input_fps)
     tiffs = convert_png_to_tiff.map(file_path=fps)
-    zarr_assets = bioformats_gen_zarr.map(file_path=fps, upstream_tasks=[tiffs])
-    thumb_assets = gen_thumb.map(file_path=fps, upstream_tasks=[zarr_assets])
+    zarr_assets = bioformats_gen_zarr.map(file_path=fps, wait_for=[tiffs])
+    thumb_assets = gen_thumb.map(file_path=fps, wait_for=[zarr_assets])
     prim_fps = utils.gen_prim_fps.map(fp_in=fps)
     callback_with_thumbs = utils.add_asset.map(prim_fp=prim_fps, asset=thumb_assets)
     callback_with_pyramids = utils.add_asset.map(
@@ -143,7 +140,16 @@ with Flow(
     )
     filtered_callback = utils.filter_results(callback_with_pyramids)
 
-    cb = utils.send_callback_body(
-        token=token, callback_url=callback_url, files_elts=filtered_callback
+    callback_state = utils.send_callback_body(
+        token=token,
+        callback_url=callback_url,
+        no_api=no_api,
+        files_elts=filtered_callback,
     )
-    rm_workdirs = utils.cleanup_workdir(fps, upstream_tasks=[cb])
+    utils.cleanup_workdir(fps, wait_for=[callback_state])
+
+    utils.log(f"callback_state = {callback_state}")
+    utils.notify_api_completion(
+        callback_state, token, callback_url, no_api, wait_for=callback_state
+    )
+    utils.log("COMPLETED")
--- test/test_lrg_2d.py
+++ test/test_lrg_2d.py
@@ -1,10 +1,11 @@
 def test_input_fname(mock_nfs_mount):
-    from em_workflows.lrg_2d_rgb.flow import flow
+    from em_workflows.lrg_2d_rgb.flow import lrg_2d_flow
 
     # monkeypatch.setattr(Config, "convert_loc", command_loc("convert"))
 
-    state = flow.run(
+    state = lrg_2d_flow(
         input_dir="/test/input_files/lrg_ROI_pngs/Projects",
         no_api=True,
+        return_state=True,
     )
-    assert state.is_successful()
+    assert state.is_completed()
