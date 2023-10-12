519508ec6639c985c19aec1089afb8fbad13668c
Anish <anish.shrestha@nih.gov>
2023-10-12 09:31:22 -0400

    Refactor lrg workflow for prefect2

--- test/test_lrg_2d.py
+++ test/test_lrg_2d.py
@@ -3,14 +3,14 @@ import pytest
 
 @pytest.mark.slow
 def test_input_fname(mock_nfs_mount, caplog):
-    from em_workflows.lrg_2d_rgb.flow import flow
+    from em_workflows.lrg_2d_rgb.flow import lrg_2d_flow
 
-    state = flow.run(
+    state = lrg_2d_flow(
         file_share="test",
         input_dir="/test/input_files/lrg_ROI_pngs/Projects",
         no_api=True,
     )
-    assert state.is_successful()
+    assert state.is_completed()
     # FIXME error documented in error_test_lrg_2d_input_fname.log
     assert "neuroglancerZarr" in caplog.text, caplog.text
 
