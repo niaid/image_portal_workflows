8f676f230d816083fc9fc7991cdc348966c6e777
Anish <anish.shrestha@nih.gov>
2023-11-16 13:29:34 -0500

    Refactor dm workflow
    Ble

--- em_workflows/dm_conversion/constants.py
+++ em_workflows/dm_conversion/constants.py
@@ -4,21 +4,31 @@ LARGE_2D = f"{LARGE_DIM}x{LARGE_DIM}"
 SMALL_2D = f"{SMALL_DIM}x{SMALL_DIM}"
 
 # List of 2D extensions we may want to process
-VALID_2D_INPUT_EXTS = [
-    "DM4",
-    "DM3",
-    "dm4",
-    "dm3",
+TIFS_EXT = [
     "TIF",
     "TIFF",
     "tif",
     "tiff",
+]
+DMS_EXT = [
+    "DM4",
+    "DM3",
+    "dm4",
+    "dm3",
+]
+JPGS_EXT = [
     "JPEG",
     "JPG",
     "jpeg",
     "jpg",
+]
+PNGS_EXT = [
     "PNG",
     "png",
+]
+MRCS_EXT = [
     "mrc",
     "MRC",
 ]
+VALID_2D_INPUT_EXTS = TIFS_EXT + DMS_EXT + JPGS_EXT + PNGS_EXT + MRCS_EXT
+KEYIMG_EXTS = TIFS_EXT + JPGS_EXT + PNGS_EXT
