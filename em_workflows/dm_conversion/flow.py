36cca50743851d361e3a03955805f09c829df2dd
Anish <anish.shrestha@nih.gov>
2023-12-13 11:24:22 -0500

    add async

--- em_workflows/dm_conversion/flow.py
+++ em_workflows/dm_conversion/flow.py
@@ -180,7 +180,7 @@ def scale_jpegs(file_path: FilePath, size: str) -> Optional[dict]:
     log_prints=True,
     task_runner=DMConfig.SLURM_EXECUTOR,
 )
-def convert_intermediate_files(fps):
+async def convert_intermediate_files(fps):
     tif_fps = [
         fp
         for fp in fps
@@ -208,7 +208,7 @@ def convert_intermediate_files(fps):
     on_completion=[utils.notify_api_completion],
     on_failure=[utils.notify_api_completion],
 )
-def dm_flow(
+async def dm_flow(
     file_share: str,
     input_dir: str,
     x_file_name: Optional[str] = None,
@@ -246,7 +246,7 @@ def dm_flow(
     # logs = utils.init_log.map(file_path=fps)
 
     # subflow calls are blocking, so lower task runs auto waits always
-    convert_intermediate_files(fps)
+    await convert_intermediate_files(fps)
 
     # Finally generate all valid suffixed results
     keyimg_assets = scale_jpegs.map(fps, size=unmapped("l"))
