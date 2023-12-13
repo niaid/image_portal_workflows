a6eafd39b04cbd6c44174a65593379b17a6296f3
Anish <anish.shrestha@nih.gov>
2023-12-13 12:10:14 -0500

    add notify api running to all

--- em_workflows/sem_tomo/flow.py
+++ em_workflows/sem_tomo/flow.py
@@ -316,6 +316,11 @@ def sem_tomo_flow(
     x_keep_workdir: bool = False,
     tilt_angle: float = 0,
 ):
+    if x_no_api:
+        utils.notify_api_running(x_no_api=x_no_api)
+    else:
+        utils.notify_api_running(token=token, callback_url=callback_url)
+
     input_dir_fp = utils.get_input_dir(share_name=file_share, input_dir=input_dir)
     # note FIBSEM is different to other flows in that it uses *directories*
     # to define stacks. Therefore, will have to list dirs to discover stacks
