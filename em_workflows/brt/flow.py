a3bb14975b33968798876e0b60a0682263f1fad6
Philip MacMenamin <philip.macmenamin@nih.gov>
2023-12-22 15:25:57 +0000

    WIP use bigger cluster

--- em_workflows/brt/flow.py
+++ em_workflows/brt/flow.py
@@ -611,7 +611,7 @@ def get_callback_result(callback_data: list) -> list:
     name="BRT",
     flow_run_name=utils.generate_flow_run_name,
     log_prints=True,
-    task_runner=BRTConfig.SLURM_EXECUTOR,
+    task_runner=BRTConfig.HIGH_SLURM_EXECUTOR,
     on_completion=[utils.notify_api_completion],
     on_failure=[utils.notify_api_completion],
 )
