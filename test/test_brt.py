d701d26a52a91c0cf9939efd537e144e55008697
Anish <anish.shrestha@nih.gov>
2023-12-28 14:37:01 -0500

    Update partially correct brt tests

--- test/test_brt.py
+++ test/test_brt.py
@@ -4,8 +4,9 @@ test_brt.py runs an end-to-end test of the batchruntomo pipeline
 
 NOTE: These tests depend on setup performed in conftest.py
 """
-import pytest
+import os
 import json
+import pytest
 from pathlib import Path
 
 
@@ -49,9 +50,18 @@ def test_brt_callback_one_valid_of_two_inputs(
     """
     from em_workflows.brt.flow import brt_flow
 
-    input_dir = "test/input_files/brt/Projects/RT_TOMO/"
+    input_dir = Path("test") / "input_files" / "brt" / "Projects" / "RT_TOMO" / "Part"
     if not Path(input_dir).exists():
         pytest.skip(f"Directory {input_dir} doesn't exist")
+
+    valid_mrc, broken_st = (
+        "2013-1220-dA30_5-BSC-1_10.mrc",
+        "broken-20130412-Vn-poxtomo_8.st",
+    )
+    assert os.listdir(input_dir) == [
+        valid_mrc,
+        broken_st,
+    ], "One valid and a broken file should have existed."
 
     result = brt_flow(
         adoc_template="plastic_brt",
@@ -65,7 +75,7 @@ def test_brt_callback_one_valid_of_two_inputs(
         LocalAlignments=0,
         THICKNESS=30,
         file_share="test",
-        input_dir=input_dir,
+        input_dir=input_dir.as_posix(),
         x_no_api=True,
         x_keep_workdir=True,
         return_state=True,
@@ -77,5 +87,5 @@ def test_brt_callback_one_valid_of_two_inputs(
         callback_output = json.load(fd)
 
     assert "files" in callback_output
-    print(callback_output["files"])
-    assert False
+    assert len(callback_output["files"]) == 1, "Only one file should have passed"
+    assert valid_mrc in callback_output["files"][0]["primaryFilePath"]
