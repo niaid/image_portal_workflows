ba48003b9f31c97290fe296caf55e1758ce0f6cf
Anish <anish.shrestha@nih.gov>
2024-01-10 10:52:11 -0500

    SEM flow refactor; use cleanup hooks over tasks

--- em_workflows/sem_tomo/flow.py
+++ em_workflows/sem_tomo/flow.py
@@ -21,11 +21,13 @@ Sem Tomo pipeline overview:
 
 import glob
 import math
+import json
 from pathlib import Path
 from typing import Dict, Optional
 from natsort import os_sorted
 
 from prefect import flow, task, unmapped
+from prefect.states import Completed, Failed
 from pytools.HedwigZarrImages import HedwigZarrImages
 
 from em_workflows.utils import utils
@@ -303,8 +305,14 @@ def gen_ng_metadata(fp_in: FilePath) -> Dict:
     flow_run_name=utils.generate_flow_run_name,
     log_prints=True,
     task_runner=SEMConfig.SLURM_EXECUTOR,
-    on_completion=[utils.notify_api_completion],
-    on_failure=[utils.notify_api_completion],
+    on_completion=[
+        utils.notify_api_completion,
+        utils.copy_workdirs_and_cleanup_hook,
+    ],
+    on_failure=[
+        utils.notify_api_completion,
+        utils.copy_workdirs_and_cleanup_hook,
+    ],
 )
 def sem_tomo_flow(
     file_share: str,
@@ -377,11 +385,23 @@ def sem_tomo_flow(
         prim_fp=callback_with_corr_mrcs, asset=corrected_movie_assets
     )
 
-    utils.callback_with_cleanup(
-        fps=fps,
-        callback_result=callback_with_corr_movies,
+    callback_result = list()
+    for cb in callback_with_corr_movies:
+        state = cb.wait()
+        try:
+            if state.is_completed():
+                json.dumps(cb.result())
+                callback_result.append(cb.result())
+        except TypeError:  # can't serialize the item
+            utils.log(f"Following item cannot be added to callback:\n\n{cb.result()}")
+
+    utils.send_callback_body.submit(
         x_no_api=x_no_api,
-        callback_url=callback_url,
         token=token,
-        x_keep_workdir=x_keep_workdir,
+        callback_url=callback_url,
+        files_elts=callback_result,
     )
+
+    if callback_result:
+        return Completed(message="At least one callback is correct!")
+    return Failed(message="None of the files succeeded!")
