9b38dd4902f92ca832f2b2534ad70718a949de1a
Anish <anish.shrestha@nih.gov>
2024-01-10 10:35:04 -0500

    LRG flow refactor; use cleanup hooks over tasks

--- em_workflows/lrg_2d_rgb/flow.py
+++ em_workflows/lrg_2d_rgb/flow.py
@@ -1,9 +1,11 @@
+import json
 from typing import Dict, Optional
 from pathlib import Path
 
 import SimpleITK as sitk
 from pytools import HedwigZarrImage, HedwigZarrImages
 from prefect import flow, task
+from prefect.states import Completed, Failed
 
 from em_workflows.utils import utils
 from em_workflows.utils import neuroglancer as ng
@@ -139,8 +141,14 @@ def gen_thumb(file_path: FilePath):
     flow_run_name=utils.generate_flow_run_name,
     log_prints=True,
     task_runner=LRG2DConfig.SLURM_EXECUTOR,
-    on_completion=[utils.notify_api_completion],
-    on_failure=[utils.notify_api_completion],
+    on_completion=[
+        utils.notify_api_completion,
+        utils.copy_workdirs_and_cleanup_hook,
+    ],
+    on_failure=[
+        utils.notify_api_completion,
+        utils.copy_workdirs_and_cleanup_hook,
+    ],
 )
 # run_config=LocalRun(labels=[utils.get_environment()]),
 def lrg_2d_flow(
@@ -183,18 +191,23 @@ def lrg_2d_flow(
         prim_fp=callback_with_thumbs, asset=zarr_assets
     )
 
-    utils.callback_with_cleanup(
-        fps=fps,
-        callback_result=callback_with_pyramids,
+    callback_result = list()
+    for cb in callback_with_pyramids:
+        state = cb.wait()
+        try:
+            if state.is_completed():
+                json.dumps(cb.result())
+                callback_result.append(cb.result())
+        except TypeError:  # can't serialize the item
+            utils.log(f"Following item cannot be added to callback:\n\n{cb.result()}")
+
+    utils.send_callback_body.submit(
         x_no_api=x_no_api,
-        callback_url=callback_url,
         token=token,
-        x_keep_workdir=x_keep_workdir,
+        callback_url=callback_url,
+        files_elts=callback_result,
     )
 
-    """
-    # TODO which of the results above "actually" determines the state of the workflow run
-    final_state = bool(filtered_callback)
-    # by definition, if any one of the file passes, it is "success"
-    return Completed(message="Success") if final_state else Failed(message="Failed")
-    """
+    if callback_result:
+        return Completed(message="At least one callback is correct!")
+    return Failed(message="None of the files succeeded!")
