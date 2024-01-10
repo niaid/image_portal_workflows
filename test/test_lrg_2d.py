9d12c54b5040c29045a25a831cf6c676c061f177
Anish <anish.shrestha@nih.gov>
2024-01-10 10:35:04 -0500

    LRG flow refactor; use cleanup hooks over tasks

--- test/test_lrg_2d.py
+++ test/test_lrg_2d.py
@@ -3,7 +3,7 @@ import pytest
 
 
 @pytest.mark.slow
-def test_input_fname(mock_nfs_mount, caplog):
+def test_lrg_2d_flow(mock_nfs_mount, caplog):
     from em_workflows.lrg_2d_rgb.flow import lrg_2d_flow
 
     state = lrg_2d_flow(
