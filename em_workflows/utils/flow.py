5b86bc323cc56ef1985a57e8d488fe7f26b75496
Anish <anish.shrestha@nih.gov>
2024-01-25 11:05:24 -0500

    Add docs for pytest workflow, remove is_run_recent check as it is scheduled

--- em_workflows/utils/flow.py
+++ em_workflows/utils/flow.py
@@ -8,9 +8,7 @@ These workflows are created simply to aid developers.
   Test input files can be of big size which can be hard to manage. Since most of the tests are normally executed on
   HPC over local machine, it is deemed okay have a prefect scheduler to run the tests for us.
 """
-from pathlib import Path
 import subprocess
-from time import time
 
 from prefect import flow, task
 from prefect.artifacts import create_markdown_artifact
@@ -18,13 +16,10 @@ from prefect.artifacts import create_markdown_artifact
 from em_workflows.config import Config
 from em_workflows.utils import utils
 
-RECENT_FILE = "coverage.svg"
-RECENT_HOUR = 1.0
-
 
 @task
-def publish_artifact(report: str):
-    return create_markdown_artifact(
+def publish_artifact(report: str) -> None:
+    create_markdown_artifact(
         key="pytest-cov-report",
         markdown=report,
         description="Pytest Coverage Report",
@@ -32,23 +27,11 @@ def publish_artifact(report: str):
 
 
 @task
-def is_run_recently():
-    """
-    Checks if the test has been run recently using locally saved file
-    """
-    path = Path(RECENT_FILE)
-    if not path.exists():
-        return False
-    last_modified = path.stat().st_mtime
-    is_recent = ((time() - last_modified) / 60) < RECENT_HOUR  # is run recently
-    utils.log(f"Pytest was run recently: {is_recent}")
-    return is_recent
-
-
-@task
 def run_tests() -> str:
     """
     Run pytest and save coverage
+    The returned string can be viewed in pytest server artifacts section
+    Currently, https://prefect2.hedwig-workflow-api.niaiddev.net/artifacts/key/pytest-cov-report
     """
     pytest_sp = subprocess.run("pytest".split(), check=False, capture_output=True)
     # "test/test_utils.py::test_task_result_persistend_and_accessed_by_hooks"
@@ -57,13 +40,17 @@ def run_tests() -> str:
     )
     if pytest_sp.stderr:
         raise RuntimeError(pytest_sp.stderr)
+    # coverage.svg is used to show the coverage percentage in the github site
     sp = subprocess.run(
-        f"coverage-badge -f -o {RECENT_FILE}".split(), check=False, capture_output=True
+        "coverage-badge -f -o coverage.svg".split(), check=False, capture_output=True
     )
     utils.log(f"Coverage is done. {sp.returncode=}\n {sp.stdout=}\n {sp.stderr=}")
     if sp.stderr:
         raise RuntimeError(sp.stderr)
     report = pytest_sp.stdout.decode()
+    # pytest returns a test report with warnings, failure details and more
+    # However, the meat of the pytest report is after the ---- coverage: pattern
+    # So we are only grabbing the main coverage report to show
     idx = report.find("---------- coverage:")
     return report[idx:]
 
@@ -73,14 +60,10 @@ def run_tests() -> str:
     log_prints=True,
     task_runner=Config.SLURM_EXECUTOR,
 )
-def pytest_flow(force_run: bool = False):
-    if not force_run:
-        is_run_recent = is_run_recently.submit().result()
-        if is_run_recent:
-            return
+def pytest_flow() -> None:
     test_report = run_tests.submit()
-    return publish_artifact.submit(test_report)
+    publish_artifact.submit(test_report)
 
 
 if __name__ == "__main__":
-    pytest_flow(True)
+    pytest_flow()
