921cd13dc018eec0f9b5dc9f72dcac909a6dd67e
Philip MacMenamin <philip.macmenamin@nih.gov>
2024-01-26 16:06:29 +0000

    Revert "dm pipeline; Remove intermediate function: Move all tasks to the @flow"
    This reverts commit 7eaabd717053d52a4ac3928bef7a6361573fe831.

--- em_workflows/sem_tomo/flow.py
+++ em_workflows/sem_tomo/flow.py
@@ -94,7 +94,7 @@ def gen_align_xg(fp_in: FilePath) -> FilePath:
     name="Newstack mrc generation",
     on_failure=[utils.collect_exception_task_hook],
 )
-def gen_newstack_combi(fp_in: FilePath, stretch: None) -> Dict:
+def gen_newstack_combi(fp_in: FilePath, stretch: None) -> tuple:
     """
     eg::
 
@@ -122,6 +122,8 @@ def gen_newstack_combi(fp_in: FilePath, stretch: None) -> Dict:
     utils.log(f"Created {cmd}")
     FilePath.run(cmd=cmd, log_file=log_file)
     assets_fp_adjusted_mrc = fp_in.copy_to_assets_dir(fp_to_cp=base_mrc)
+    # (to remove the need of wait_for in downstream)
+    # returning the asset as well as the input fp
     return fp_in.gen_asset(
         asset_type=AssetType.AVERAGED_VOLUME, asset_fp=assets_fp_adjusted_mrc
     )
