72552c600c833c7f58200b4d130e8248ee70ddb9
Philip MacMenamin <philip.macmenamin@nih.gov>
2024-01-26 16:06:29 +0000

    Revert "Refactor dm workflow"
    This reverts commit fea00273a7912dde8afc13bfc1b291e474bef64e.

--- em_workflows/dm_conversion/constants.py
+++ em_workflows/dm_conversion/constants.py
@@ -4,31 +4,21 @@ LARGE_2D = f"{LARGE_DIM}x{LARGE_DIM}"
 SMALL_2D = f"{SMALL_DIM}x{SMALL_DIM}"
 
 # List of 2D extensions we may want to process
-TIFS_EXT = [
-    "TIF",
-    "TIFF",
-    "tif",
-    "tiff",
-]
-DMS_EXT = [
+VALID_2D_INPUT_EXTS = [
     "DM4",
     "DM3",
     "dm4",
     "dm3",
-]
-JPGS_EXT = [
+    "TIF",
+    "TIFF",
+    "tif",
+    "tiff",
     "JPEG",
     "JPG",
     "jpeg",
     "jpg",
-]
-PNGS_EXT = [
     "PNG",
     "png",
-]
-MRCS_EXT = [
     "mrc",
     "MRC",
 ]
-VALID_2D_INPUT_EXTS = TIFS_EXT + DMS_EXT + JPGS_EXT + PNGS_EXT + MRCS_EXT
-KEYIMG_EXTS = TIFS_EXT + JPGS_EXT + PNGS_EXT
