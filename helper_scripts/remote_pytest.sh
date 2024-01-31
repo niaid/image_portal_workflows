dfc531fe3dcb0505cbd9df7fd7f3c1cb96f9c1f1
Anish <anish.shrestha@nih.gov>
2024-01-31 15:12:19 -0500

    Check vpn connection

--- helper_scripts/remote_pytest.sh
+++ helper_scripts/remote_pytest.sh
@@ -6,11 +6,19 @@ if [[ ! $(prefect config view | grep PREFECT_API_KEY) ]]; then
   exit 1;
 fi
 
+base_url="https://prefect2.hedwig-workflow-api.niaiddev.net";
+api_url="$base_url/api";
+echo
+if [[ ! $(curl $api_url/health | grep "true") ]]; then
+  echo
+  echo "Make sure you are connected to NIAID VPN";
+  exit 1;
+fi
+
 branch=`git branch --show-current`
 echo "Sending pytest runner for branch=$branch";
-echo "Make sure you are connected to NIAID VPN";
-echo
 
+echo
 if [[ ! $(git branch -r | grep $branch) ]]; then
   echo "Push the branch to remote before executing this action.";
   echo
@@ -21,12 +29,12 @@ else
 fi
 
 run_id=$(curl -i -X POST \
-  "https://prefect2.hedwig-workflow-api.niaiddev.net/api/deployments/d87fc0e8-5c71-408a-bcbe-f2a6320686d1/create_flow_run" \
+  "$api_url/deployments/d87fc0e8-5c71-408a-bcbe-f2a6320686d1/create_flow_run" \
   -H "accept: application/json" -H "Content-Type: application/json" -H "Authorization: Bearer $PREFECT_API_KEY" \
   -d "{\"parameters\": {\"git_branch\":\"$branch\"}}" | \
   sed -n 's/.*\([0-9a-f]\{8\}-[0-9a-f]\{4\}-[0-9a-f]\{4\}-[0-9a-f]\{4\}-[0-9a-f]\{12\}\).*/\1/p')
 
-flow="https://prefect2.hedwig-workflow-api.niaiddev.net/flow-runs/flow-run/$run_id"
+flow="$base_url/flow-runs/flow-run/$run_id"
 echo
 echo "Flow Run can be accessed here: $flow";
 echo
