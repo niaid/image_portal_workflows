f3976db5835c765b7ec47df8e639da7f9e47c39a
Philip MacMenamin <philip.macmenamin@nih.gov>
2025-09-09 22:32:22 +0100

    make test report as passed if fails appropriately

--- test/test_czi.py
+++ test/test_czi.py
@@ -35,13 +35,12 @@ async def test_no_mount_point_flow_fails(mock_binaries, monkeypatch, caplog):
 
     monkeypatch.setattr(config, "NFS_MOUNT", _mock_NFS_MOUNT)
 
-    with pytest.raises(UnfinishedRun):
+    with pytest.raises(RuntimeError, match=f"{share_name} doesn't exist. Failing!"):
         await czi_flow(
             file_share=share_name,
             input_dir="test/input_files/IF_czi/Projects/Cropped_Image/",
             x_no_api=True,
         )
-    assert f"{share_name} doesn't exist. Failing!" in caplog.text, caplog.text
 
 
 @pytest.mark.asyncio
