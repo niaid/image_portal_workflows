1402feccf4b3c955e5898b131d886641baddb8ad
Philip MacMenamin <philip.macmenamin@nih.gov>
2026-06-25 12:30:19 -0400

    update py version to 3.14, update prefect to 3.7

--- setup.py
+++ setup.py
@@ -1,26 +1,6 @@
 #!/usr/bin/env python
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
-with open("requirements.txt", "r") as fp:
-    requirements = list(filter(bool, (line.strip() for line in fp)))
-
-setup(
-    name="em_workflows",
-    version="2.1.2",
-    author="Philip MacMenamin, Bradley Lowekamp, Anish Shrestha",
-    author_email="bioinformatics@niaid.nih.gov",
-    description="Workflows for Microscopy related file processing.",
-    url="https://www.niaid.nih.gov/research/bioinformatics-computational-biosciences-branch",
-    license="BSD 3-Clause.",
-    classifiers=[
-        "Development Status :: Release",
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: BSD 3-Clause.",
-        "Operating System :: OS Independent",
-    ],
-    python_requires=">=3.8",
-    install_requires=requirements,
-    packages=find_packages(include=["em_workflows", "em_workflows.*"]),
-    package_data={"em_workflows": ["templates/*.adoc"]},
-)
+# Metadata and dependencies are declared in pyproject.toml.
+setup()
